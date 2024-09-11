f12bab219e6374b7b0c0e0bd521ef4e9249d0196
mjkim <mk8831@nyu.edu>
2024-09-10 22:51:37 -0500

    update answers

--- gettingStarted.py
+++ gettingStarted.py
@@ -3,29 +3,31 @@ def welcome_assignment_answers(question):
 
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
-    elif question == "Are encoding and encryption the same?":
+    elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
-    elif question == "Is it possible to decrypt a message without a key?":
-        answer = "No"
-    elif question == "Is it possible to decode a message without a key?":
-        answer = "No"
-    elif question == "Is a hashed message supposed to be un-hashed?":
-        answer = "No"
-    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code":
-        answer = "dont know yet"
-    elif question == "Is MD5 a secured hashing algorithm?":
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to?":
-        answer = "don't know yet"
-    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to?":
-        answer = "don't know yet"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "dont know yet"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "4"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "3"
     else:
         answer = "Not a valid question!"
 
     return(answer)
 
-print(welcome_assignment_answers(input()))
-print("Hello")
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same?"
+    print(welcome_assignment_answers(debug_question))
 
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
